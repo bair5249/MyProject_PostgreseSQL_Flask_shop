af47762f5668b216587bf38616b015fd5042a606
bir5249 <bair5249@yandex.ru>
2024-03-02 12:06:29 +0300

    added flask without database

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/home')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
